5c883de1293c98dd86678c53398262ea079beec2
arpitag1 <34940466+arpitag1@users.noreply.github.com>
2022-11-25 12:35:39 +0000

    Update code2.py

--- code2.py
+++ code2.py
@@ -42,10 +42,20 @@ dataframe = pd.DataFrame(
     columns=('col %d' % i for i in range(20)))
 st.table(dataframe)
 
-"""
-
 import streamlit as st
 st.text_input("Your name", key="name")
 
 # You can access the value at any point with:
 st.session_state.name
+
+"""
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    chart_data
