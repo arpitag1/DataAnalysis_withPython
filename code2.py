2675a12acf1849ab2e73f1f3a32b24e5b4090bc2
arpitag1 <34940466+arpitag1@users.noreply.github.com>
2022-11-25 12:39:57 +0000

    Update code2.py

--- code2.py
+++ code2.py
@@ -58,7 +58,7 @@ if st.checkbox('Show dataframe'):
        columns=['a', 'b', 'c'])
 
     chart_data
-"""
+
 import streamlit as st
 import pandas as pd
 
@@ -72,3 +72,17 @@ option = st.selectbox(
      df['first column'])
 
 'You selected: ', option
+"""
+import streamlit as st
+
+# Add a selectbox to the sidebar:
+add_selectbox = st.sidebar.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone')
+)
+
+# Add a slider to the sidebar:
+add_slider = st.sidebar.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
